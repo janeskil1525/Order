2258d635570297fc92e6b2f3becb3ec677b73389
janeskil1525 <janeskil1525@gmail.com>
2020-10-15 18:03:54 +0200

    XML Base

---
+++ lib/Order/Helper/Orion/Communicator/BaseXML.pm
@@ -0,0 +1,12 @@
+package Order::Helper::Orion::Communicator::BaseXML;
+use Mojo::Base 'Daje::Utils::Sentinelsender';
+
+use Data::Dumper;
+use HTTP::Headers;
+use XML::Hash::XS;
+use LWP::UserAgent;
+use HTTP::Request;
+
+
+
+1;
